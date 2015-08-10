dd7face1c1dd03a4ca4f35c8289173c88857fbec
tpetzoldt <thomas.petzoldt@tu-dresden.de>
2015-08-10 23:58:10 +0200

    Baranyi model added

---
+++ examples/example_baranyi.R
@@ -0,0 +1,60 @@
+## Test of the Baranyi Growth model, cf. Baranyi, L (1995), Food Microbiology
+##
+## Note: the original model formulation works in log space, so that
+##       y(t), y_0 and y_max are all given as natural log.
+##  This is advantageous for model fitting, but here we use y_0 and K in 
+##  untransformed space to be compatible with the growth parameters of
+##  most other models. The downside is, that we need box constraints in most
+##  cases and possibly more iterations.
+##
+##  A version with log-transformed parameters may follow ...
+
+
+library("growthrates")
+library("lattice")
+
+## load data
+data(bactgrowth)
+splitted.data <- multisplit(bactgrowth, c("strain", "conc", "replicate"))
+dat <- splitted.data[[23]]
+
+## initial parameters and bocx constraints
+p   <- c(y0=0.03, mu=.1, K=0.1, h0=1)
+
+lower   <- c(y0=0.001, mu=1e-2, K=0.005, h0=0)
+upper   <- c(y0=0.1,   mu=1,    K=0.5,   h0=10)
+
+## fit model
+fit <- fit_growthmodel(FUN=grow_baranyi, p=p, time=dat$time, y=dat$value,
+                       lower = lower, upper = upper,
+                       control=list(nprint=TRUE))
+
+## coefficients and plot
+coef(fit)
+plot(fit)
+
+
+## fit growth models to all data using (log transformed residuals)
+L <- all_growthmodels(grow_baranyi, p=p, df=bactgrowth, 
+                      criteria = c("strain", "conc", "replicate"),
+                      lower = lower, upper=upper, 
+                      log="y")
+
+
+## fit growth models to all data using (log transformed residuals)
+p   <- c(y0=0.01, mu=.1, K=0.1, h0=0.65) # 0.65 was mean
+L <- all_growthmodels(grow_baranyi, p=p, df=bactgrowth, 
+                      criteria = c("strain", "conc", "replicate"),
+                      which=c("y0", "mu", "K"),
+                      lower = lower, upper=upper)
+
+
+par(mfrow=c(4,3))
+par(mar=c(2.5,4,2,1))
+plot(L, log="y")
+
+par(mfrow=c(4,3))
+plot(L)
+
+res <- results(L)
+xyplot(mu ~ log(conc + 1)| strain, data=res)
---
+++ pkg/R/grow_baranyi.R
@@ -0,0 +1,52 @@
+#' The Baranyi and Roberts Growth Model
+#'
+#' The growth model of Baranyi and Roberts (1995) written as analytical solution 
+#' of the system of differential equations.
+#'
+#' @param time vector of time steps (independend variable)
+#' @param parms named parameter vector of the logistic growth model with:
+#' \itemize{
+#'   \item \code{y0} initial value of population measure
+#'   \item \code{mu} maximum growth rate (1/time)
+#'   \item \code{K} carrying capacity (max. total concentration of cells)
+#'   \item \code{h0} parameter specifying the initial physiological state of 
+#'     the cells and in consequence the lag phase 
+#'     (h0 approx max growth rate * lag phase).
+#' }
+#'
+#' @return vector of dependend variable (\code{y})
+#' 
+#' 
+#' 
+#' @references 
+#' 
+#' Baranyi, J. and Roberts, T. A. (1994). 
+#' A dynamic approach to predicting bacterial growth in food.
+#' International Journal of Food Microbiology, 23, 277-294.
+#' 
+#' Baranyi, J. and Roberts, T.A. (1995). Mathematics of predictive microbiology. 
+#' International Journal of Food Microbiology, 26, 199-218.
+#' 
+#'
+#' @examples
+#'
+#' time <- seq(0, 30, length=200)
+#' y    <- grow_baranyi(time, c(y0=0.01, mu=.5, K=0.1, h0=5))[,"y"]
+#' plot(time, y, type="l")
+#' plot(time, y, type="l", log="y")
+#'
+#' @family growth models
+#'
+#' @rdname grow_baranyi
+#' @export grow_baranyi
+#'
+grow_baranyi <- function(time, parms) {
+  with(as.list(parms), {
+    ## todo: q0 in original paper, h0 in Huang
+    A <- time + 1/mu * log(exp(-mu * time) + exp(-h0) - exp(-mu * time - h0))
+    #log_y <- y0 + mu * A - log(1 + (exp(mu * A) - 1)/(exp(K - y0)))
+    log_y <- log(y0) + mu * A - log(1 + (exp(mu * A) - 1) / exp(log(K) - log(y0)))
+    
+    return(as.matrix(data.frame(time = time, y = exp(log_y), log_y = log_y)))
+  })
+}
