5e828b68eaf7d6c5ccc735354a24d3d64c29eb91
thpe <thomas.petzoldt@tu-dresden.de>
2016-03-12 14:45:08 +0100

    examples

---
+++ pkg/inst/doc/examples/examples_compiled_ode.R
@@ -0,0 +1,44 @@
+## This script demonstrates the performance gain 
+## from using compiled versions of the ODE models
+
+library(growthrates)
+
+
+### Two-Step growth model (2 ODE equations)
+## R code
+system.time(for (i in 1:100)
+  o1 <-
+    grow_twostep.R(0:100, c(
+      yi = 0.01, ya = 0.0, kw = 0.1,	mumax = 0.2, K = 0.1
+    )))
+
+## compiled C code
+system.time(for (i in 1:100)
+  o2 <-
+    grow_twostep(0:100, c(
+      yi = 0.01, ya = 0.0, kw = 0.1,	mumax = 0.2, K = 0.1
+    )))
+
+### extended logistic growth model (2 ODE equations)
+## R code
+system.time(for (i in 1:100)
+  o3 <-
+    grow_genlogistic.R(0:100, c(
+      y0 = 0.1, mumax = 0.5, K = 10, alpha = 1.2, beta = 1.2, gamma = 1.2
+    )))
+
+## compiled C code
+system.time(for (i in 1:100)
+  o4 <-
+    grow_genlogistic(0:100, c(
+      y0 = 0.1, mumax = 0.5, K = 10, alpha = 1.2, beta = 1.2, gamma = 1.2
+    )))
+
+
+## check if results are identical
+summary(o1 - o2)
+
+summary(o3 - o4)
+
+
+
