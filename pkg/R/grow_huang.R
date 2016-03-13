272b3a00abf7589067216f256b3eab4117e6e2cd
thpe <thomas.petzoldt@tu-dresden.de>
2016-03-13 21:05:39 +0100

    rm URL with spaces

--- pkg/R/grow_huang.R
+++ pkg/R/grow_huang.R
@@ -46,7 +46,6 @@
 #' Huang, Lihan (2013) Introduction to USDA Integrated Pathogen Modeling
 #' Program (IPMP). Residue Chemistry and Predictive Microbiology Research
 #' Unit. USDA Agricultural Research Service.
-#' http://www.ars.usda.gov/SP2UserFiles/Place/80720500/IPMP_tutorial%201-9-14.pdf
 #'
 #'
 #'
