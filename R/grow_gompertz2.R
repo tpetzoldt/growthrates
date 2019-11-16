6b48986f7745064f95b7cfcb1d30ec0a493cf257
thpe <thomas.petzoldt@tu-dresden.de>
2019-11-16 16:37:38 +0100

    rebuild documentation

--- R/grow_gompertz2.R
+++ R/grow_gompertz2.R
@@ -35,7 +35,7 @@
 #'
 #' time <- seq(0, 30, length=200)
 #' y    <- grow_gompertz(time, c(y0=1, mumax=.2, K=10))[,"y"]
-#' plot(time, y, type="l", ylim=c(0, 20))
+#' plot(time, y, type="l", ylim=c(0, 12))
 #'
 #'
 #' @family growth models
